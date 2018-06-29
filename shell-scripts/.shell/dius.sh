be13d7db5328108e5d0859c0c11d8608dd7e7513
bangn <thebangnguyen@gmail.com>
2018-06-29 15:18:18 +1000

    zsh: Add script to update local git config

--- shell-scripts/.shell/dius.sh
+++ shell-scripts/.shell/dius.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 # shellcheck disable=SC2164
+
 cdd() {
   cd "$HOME/dev/dius/$1"
 }
 
 cdn() {
   cd "$HOME/dev/dius/nib/$1"
+}
+
+update_git_user_config() {
+  cat << EOF | tee -a ./.git/config
+[user]
+    name = bangn
+    email = bnguyen@dius.com.au
+EOF
 }
