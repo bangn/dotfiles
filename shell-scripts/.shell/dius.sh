49b4b59428590f897e1371b36384646dd8e83397
bangn <thebangnguyen@gmail.com>
2018-07-03 13:48:51 +1000

    zsh: Update script to update local git config

--- shell-scripts/.shell/dius.sh
+++ shell-scripts/.shell/dius.sh
@@ -10,9 +10,35 @@ cdn() {
 }
 
 update_git_user_config() {
-  cat << EOF | tee -a ./.git/config
+  config_file="./.git/config"
+
+  if [[ -f "$config_file" ]] && ! grep -q "\[user\]" "$config_file"; then
+    cat << EOF | tee -a "$config_file"
 [user]
-    name = bangn
-    email = bnguyen@dius.com.au
+  name = bangn
+  email = bnguyen@dius.com.au
 EOF
+  fi
+}
+
+update_git_host_config() {
+  config_file="./.git/config"
+  host="github.com"
+
+  if [[ "$PWD" =~ nib ]]; then
+    host="git.nib.com.au"
+  fi
+
+  if [[ -f "$config_file" ]] && ! grep -q "\[host\]" "$config_file"; then
+    cat << EOF | tee -a "$config_file"
+[host]
+  hub = "$host"
+  protocal = ssh
+EOF
+  fi
+}
+
+update_local_git_config() {
+  update_git_user_config
+  update_git_host_config
 }
