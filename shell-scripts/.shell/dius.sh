0263b6f52b1cd731f02f0e019d442f03da890a6c
bangn <thebangnguyen@gmail.com>
2019-11-02 01:17:46 +1100

    zsh: Add script to start/stop niss servers

--- shell-scripts/.shell/dius.sh
+++ shell-scripts/.shell/dius.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 # shellcheck disable=SC2164
 
+################################################################################
+# DiUs
+################################################################################
 cdd() {
   cd "$HOME/dev/dius/$1"
-}
-
-cdn() {
-  cd "$HOME/dev/dius/nib/$1" && git_update_local_config
 }
 
 git_update_user_config() {
@@ -19,6 +18,14 @@ git_update_user_config() {
   email = bnguyen@dius.com.au
 EOF
   fi
+}
+
+########################################
+# nib
+########################################
+
+cdn() {
+  cd "$HOME/dev/dius/nib/$1" && git_update_local_config
 }
 
 git_update_hub_config() {
@@ -41,4 +48,43 @@ EOF
 git_update_local_config() {
   git_update_user_config
   git_update_hub_config
+}
+
+####################
+# niss
+####################
+
+# Theses functions is tightly couple with tmux
+# I dont care much about the coupling here as tmux is used daily in my workflow
+nissup() {
+  local up_with="$1"
+  local session=niss
+  local ignore
+
+  if [ -z "$up_with" ] || [ "$up_with" = "ui" ]; then
+    ignore="niss-admin"
+  else
+    ignore="niss-ui"
+  fi
+
+  for pane in $(tmux list-panes -t "$session" -F "#{pane_current_path}:#W.#P" | grep -v "$ignore"); do
+    paneId=$(echo "$pane" | awk -F ":" '{print $2}')
+    if [[ "$pane" =~ "nailgun" ]]; then
+      tmux send-keys -t "$paneId" "be rackup -p 8080" Enter
+    else
+      tmux send-keys -t "$paneId" "npm run start.dev" Enter
+    fi
+  done
+
+  echo "niss servers are starting up"
+}
+
+nissdown() {
+  local session=niss
+  for pane in $(tmux list-panes -t "$session" -F "#{pane_current_path}:#W.#P"); do
+    paneId=$(echo "$pane" | awk -F ":" '{print $2}')
+    tmux send-keys -t "$paneId" C-C
+  done
+
+  echo "niss servers are down"
 }
