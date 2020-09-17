6227a8e6274a2974badd8b1d79a2e11302d9d552
bangn <thebangnguyen@gmail.com>
2020-09-17 11:53:50 +1000

    fixup! nix: Use nix to handle all configs

--- nixpkgs/dotfiles/default.nix
+++ nixpkgs/dotfiles/default.nix
@@ -1,36 +1,36 @@
 { pkgs, ... }:
 {
-  ".ctags".source = ./ctags/ctags;
-  ".editrc".source = ./editrc;
+  ".ctags".source = ./ctags/.ctags;
+  ".editrc".source = ./.editrc;
   ".gitmessage.sample".source = ./git/gitmessage.sample;
-  ".inputrc".source = ./inputrc;
-  ".psqlrc".source = ./psql/psqlrc;
-  ".pylintrc".source = ./python/pylintrc;
-  ".slate".source = ./slate/slate;
-  ".xsessionrc".source = ./xsession/xsessionrc;
+  ".inputrc".source = ./.inputrc;
+  ".psqlrc".source = ./psql/.psqlrc;
+  ".pylintrc".source = ./python/.pylintrc;
+  ".slate".source = ./slate/.slate;
+  ".xsessionrc".source = ./xsession/.xsessionrc;
 
   # xinput
-  ".xinitrc".source = ./xinput/xinitrc;
-  ".xinputrc".source = ./xinput/xinputrc;
+  ".xinitrc".source = ./xinput/.xinitrc;
+  ".xinputrc".source = ./xinput/.xinputrc;
 
   # Ruby dotfiles
-  ".irbrc".source = ./ruby/irbrc;
-  ".pryrc".source = ./ruby/pryrc;
-  ".rspec".source = ./ruby/rspec;
-  ".eslintrc.yml".source = ./js/eslintrc.yml;
+  ".irbrc".source = ./ruby/.irbrc;
+  ".pryrc".source = ./ruby/.pryrc;
+  ".rspec".source = ./ruby/.rspec;
+  ".eslintrc.yml".source = ./js/.eslintrc.yml;
 
   # asdf dotfiles
-  ".asdfrc".source = ./asdf/asdfrc;
-  ".default-gems".source = ./asdf/default-gems;
-  ".default-npm-packages".source = ./asdf/default-npm-packages;
-  ".tool-versions".source = ./asdf/tool-versions;
+  ".asdfrc".source = ./asdf/.asdfrc;
+  ".default-gems".source = ./asdf/.default-gems;
+  ".default-npm-packages".source = ./asdf/.default-npm-packages;
+  ".tool-versions".source = ./asdf/.tool-versions;
 
   # haskell dotfiles
-  ".ghci".source = ./haskell/ghci;
-  ".haskeline".source = ./haskell/haskeline;
-  ".hindent.yaml".source = ./haskell/hindent.yaml;
-  ".stylish-haskell.yaml".source = ./haskell/stylish-haskell.yaml;
-  ".summoner.toml".source = ./haskell/summoner.toml;
+  ".ghci".source = ./haskell/.ghci;
+  ".haskeline".source = ./haskell/.haskeline;
+  ".hindent.yaml".source = ./haskell/.hindent.yaml;
+  ".stylish-haskell.yaml".source = ./haskell/.stylish-haskell.yaml;
+  ".summoner.toml".source = ./haskell/.summoner.toml;
   "floskell.json".source = ./haskell/floskell.json;
 
   # tmuxinantor
