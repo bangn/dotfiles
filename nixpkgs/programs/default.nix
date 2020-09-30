53392124fe8cf513117dc0408c69ee897ebc3809
bangn <thebangnguyen@gmail.com>
2020-09-30 14:39:44 +1000

    nix: Add firefox settings

--- nixpkgs/programs/default.nix
+++ nixpkgs/programs/default.nix
@@ -12,5 +12,23 @@
   firefox = {
     enable = true;
     package = pkgs.firefox-bin;
+    profiles = {
+      bangn = {
+        id = 0;
+        isDefault = true;
+        settings = {
+          "browser.display.use_document_fonts" = 0;
+          "browser.uidensity" = 1;
+          "browser.urlbar.placeholderName" = "DuckDuckGo";
+          "browser.urlbar.placeholderName.private" = "DuckDuckGo";
+          "devtools.theme" = "dark";
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "font.name.monospace.x-western" = "Hack";
+          "font.name.sans-serif.x-western" = "Hack";
+          "font.name.serif.x-western" = "Hack";
+          "signon.rememberSignons" = false;
+        };
+      };
+    };
   };
 }
