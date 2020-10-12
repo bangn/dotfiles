426e9ac3fd645711a0ab108c380cc56e40b157f4
bangn <thebangnguyen@gmail.com>
2020-10-12 23:36:06 +1100

    mdl: Fix rule

--- homedot/mdl/style.rb
+++ homedot/mdl/style.rb
@@ -2,5 +2,7 @@
 
 all
 
-rule 'MD013', line_length: 300
-rule 'MD24', allow_different_nesting: true
+exclude_rule 'MD004'
+exclude_rule 'MD013'
+exclude_rule 'MD041'
+rule 'MD024', allow_different_nesting: true
