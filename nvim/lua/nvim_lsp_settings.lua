6717d01b248803a12cec7cc9788d44dbf96df756
bangn <thebangnguyen@gmail.com>
2021-02-02 09:54:58 +1100

    vim: Minor refactor/update mapping

--- nvim/lua/nvim_lsp_settings.lua
+++ nvim/lua/nvim_lsp_settings.lua
@@ -2,16 +2,17 @@ local lspconfig = require('lspconfig')
 
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
   -- Mappings.
   local opts = { noremap=true, silent=true }
-  vim.api.nvim_buf_set_keymap(bufnr , 'n' , '<C-]>'      , '<cmd>lua vim.lsp.buf.definition()<CR>'             , opts)
-  vim.api.nvim_buf_set_keymap(bufnr , 'n' , '<leader>e'  , '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>' , opts)
-  vim.api.nvim_buf_set_keymap(bufnr , 'n' , 'gD'         , '<cmd>lua vim.lsp.buf.declaration()<CR>'            , opts)
-  vim.api.nvim_buf_set_keymap(bufnr , 'n' , 'gi'         , '<cmd>lua vim.lsp.buf.implementation()<CR>'         , opts)
-  vim.api.nvim_buf_set_keymap(bufnr , 'n' , 'gr'         , '<cmd>lua vim.lsp.buf.references()<CR>'             , opts)
-  vim.api.nvim_buf_set_keymap(bufnr , 'n' , 'gt'         , '<cmd>lua vim.lsp.buf.type_definition()<CR>'        , opts)
-  vim.api.nvim_buf_set_keymap(bufnr , 'n' , 'K'          , '<cmd>lua vim.lsp.buf.hover()<CR>'                  , opts)
+  buf_set_keymap('n', '<C-]>'    , '<cmd>lua vim.lsp.buf.definition()<CR>'            , opts)
+  buf_set_keymap('n', '<leader>e', '<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>', opts)
+  buf_set_keymap('n', 'gD'       , '<cmd>lua vim.lsp.buf.declaration()<CR>'           , opts)
+  buf_set_keymap('n', 'gi'       , '<cmd>lua vim.lsp.buf.implementation()<CR>'        , opts)
+  buf_set_keymap('n', 'gr'       , '<cmd>lua vim.lsp.buf.references()<CR>'            , opts)
+  buf_set_keymap('n', 'gt'       , '<cmd>lua vim.lsp.buf.type_definition()<CR>'       , opts)
+  buf_set_keymap('n', 'K'        , '<cmd>lua vim.lsp.buf.hover()<CR>'                 , opts)
 end
 
 local servers = {
@@ -21,6 +22,8 @@ local servers = {
   'terraformls',
   'tsserver',
   'yamlls',
+  'html',
+  'dockerls',
 }
 
 for _, lsp in ipairs(servers) do
