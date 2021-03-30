2d18d7d2af04820fe02d680674e663966f4d36cc
bangn <thebangnguyen@gmail.com>
2021-03-31 06:04:44 +1100

    vim: Do not show virtual text
    Show diagnostics on hover instead.

--- nvim/lua/nvim_lsp_handler.lua
+++ nvim/lua/nvim_lsp_handler.lua
@@ -1,17 +1,58 @@
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- This will disable virtual text, like doing:
-    -- let g:diagnostic_enable_virtual_text = 0
-    virtual_text = true,
-
-    -- This is similar to:
-    -- let g:diagnostic_show_sign = 1
-    -- To configure sign display,
-    --  see: ":help vim.lsp.diagnostic.set_signs()"
-    signs = false,
-
-    -- This is similar to:
-    -- "let g:diagnostic_insert_delay = 1"
-    update_in_insert = false,
+vim.api.nvim_call_function('sign_define',
+  {
+    'LspDiagnosticsSignError',
+    {
+      text = '⤫',
+      texthl = 'LspDiagnosticsError'
+    }
   }
 )
+
+vim.api.nvim_call_function('sign_define',
+  {
+    'LspDiagnosticsSignWarning',
+    {
+      text = '⚠',
+      texthl = 'LspDiagnosticsSignWarning'
+    }
+  }
+)
+
+vim.api.nvim_call_function('sign_define',
+  {
+    'LspDiagnosticsSignInformation',
+    {
+      text = 'I',
+      texthl = 'LspDiagnosticsSignInformation'
+    }
+  }
+)
+
+vim.api.nvim_call_function('sign_define',
+  {
+    'LspDiagnosticsSignHint',
+    {
+      text = '⚡',
+      texthl = 'LspDiagnosticsSignHint'
+    }
+  }
+)
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    signs = function(bufnr, client_id)
+      local ok, result = pcall(vim.api.nvim_buf_get_var, bufnr, 'show_signs')
+      -- No buffer local variable set, so just enable by default
+      if not ok then
+        return true
+      end
+      return result
+    end,
+    underline = true,
+    update_in_insert = false,
+    virtual_text = false,
+  }
+)
+vim.cmd [[ sign define LspDiagnosticsErrorSign text= texthl=LspDiagnosticsError linehl= numhl= ]]
+vim.cmd [[autocmd CursorHold * lua require("lspsaga.diagnostic").show_line_diagnostics()]]
+vim.cmd [[autocmd CursorHoldI * silent! lua require("lspsaga.signaturehelp").signature_help()]]
--- nvim/lua/nvim_lsp_settings.lua
+++ nvim/lua/nvim_lsp_settings.lua
@@ -1,5 +1,26 @@
 local lspconfig = require('lspconfig')
-require('lspsaga').init_lsp_saga()
+require('lspsaga').init_lsp_saga({
+  use_saga_diagnostic_sign = false,
+  error_sign = '⤫',
+  warn_sign = '⚠',
+  code_action_keys = {
+    quit = '<esc>',
+    exec = '<CR>',
+  },
+  rename_action_keys = {
+    quit = '<esc>',
+    exec = '<CR>',
+  },
+  finder_action_keys = {
+    open = '<CR>',
+    vsplit = '<C-v>',
+    split = '<C-x>',
+    quit = '<esc>',
+    scroll_down = '<C-n>',
+    scroll_up = '<C-p>',
+  },
+  code_action_icon = '💡',
+})
 
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -17,8 +38,8 @@ local on_attach = function(_, bufnr)
   buf_set_keymap('n', '<leader>ca', '<cmd>lua require("lspsaga.codeaction").code_action()<CR>', opts)
   buf_set_keymap('v', '<leader>ca', '<cmd>"<,">lua require("lspsaga.codeaction").range_code_action()<CR>', opts)
   buf_set_keymap('n', 'K', '<cmd>lua require("lspsaga.hover").render_hover_doc()<CR>', opts)
-  buf_set_keymap('n', '<C-f>', '<cmd>lua require("lspsaga.hover").smart_scroll_with_saga(1)<CR>', opts)
-  buf_set_keymap('n', '<C-b>', '<cmd>lua require("lspsaga.hover").smart_scroll_with_saga(-1)<CR>', opts)
+  buf_set_keymap('n', '<C-f>', '<cmd>lua require("lspsaga.action").smart_scroll_with_saga(1)<CR>', opts)
+  buf_set_keymap('n', '<C-b>', '<cmd>lua require("lspsaga.action").smart_scroll_with_saga(-1)<CR>', opts)
   buf_set_keymap('n', 'gS', '<cmd>lua require("lspsaga.signaturehelp").signature_help()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua require("lspsaga.rename").rename()<CR>', opts)
   buf_set_keymap('n', 'gd', '<cmd>lua require"lspsaga.provider".preview_definition()<CR>', opts)
