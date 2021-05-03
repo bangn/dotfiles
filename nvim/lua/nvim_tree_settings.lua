c8d49fac3f993c3318e9e3af552c0a7fdd0ed036
bangn <thebangnguyen@gmail.com>
2021-05-03 13:12:40 +1000

    vim: Increase nvim tree size

--- nvim/lua/nvim_tree_settings.lua
+++ nvim/lua/nvim_tree_settings.lua
@@ -1,7 +1,7 @@
 local g = vim.g
 
 g.nvim_tree_side = "left";
-g.nvim_tree_width = 25;
+g.nvim_tree_width = 40;
 g.nvim_tree_ignore = {".git", "node_modules", ".cache"};
 g.nvim_tree_auto_open = 0;
 g.nvim_tree_auto_close = 1;
