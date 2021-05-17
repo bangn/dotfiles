85dc46d8b7d0dc66216bb1c9a085057d494c5b91
bangn <thebangnguyen@gmail.com>
2021-05-17 22:53:34 +1000

    vim: Update diffview, zen mode settings

--- nvim/lua/zen_mode_settings.lua
+++ nvim/lua/zen_mode_settings.lua
@@ -1,24 +1,16 @@
 local api = vim.api;
 
-function toggleZenmode()
- require("zen-mode").toggle({
-    window = {
-      width = 1,
-      height = 1,
-    }
-  })
-end
-
 local mapOpts = { noremap=true, silent=true };
-
-api.nvim_set_keymap('n', '<leader>z', '<cmd>lua toggleZenmode()<CR>', mapOpts);
+api.nvim_set_keymap('n', '<leader>z', '<cmd>lua  require("zen-mode").toggle({})<CR>', mapOpts);
 
 require("zen-mode").setup {
   window = {
     backdrop = 0.95,
+    width = 1,
+    height = 1,
   },
   plugins = {
-    gitsigns = true, -- disables git signs
+    gitsigns = false, -- disables git signs
     tmux = false, -- disables the tmux statusline
   },
 }
