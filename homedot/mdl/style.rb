1b61faf1e49dd737d5bdc2bf63ec428a4bae8968
bangn <thebangnguyen@gmail.com>
2021-07-22 12:27:53 +1000

    vim: Update imposed styles

--- homedot/mdl/style.rb
+++ homedot/mdl/style.rb
@@ -2,8 +2,9 @@
 
 all
 
-exclude_rule 'MD004'
-exclude_rule 'MD013'
-exclude_rule 'MD032'
-exclude_rule 'MD041'
-rule 'MD024', allow_different_nesting: true
+exclude_rule 'MD004' # Unordered list style
+exclude_rule 'MD013' # Line length
+exclude_rule 'MD032' # Lists should be surrounded by blank lines
+exclude_rule 'MD033' # No inline html
+
+rule 'MD024', allow_different_nesting: true # Multiple headers with the same content
