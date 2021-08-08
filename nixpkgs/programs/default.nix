cef442a6ed8fada0e1abde9785759f3c484f6241
bangn <thebangnguyen@gmail.com>
2021-08-09 00:07:55 +1000

    nix: Try broot

--- nixpkgs/programs/default.nix
+++ nixpkgs/programs/default.nix
@@ -4,6 +4,10 @@
   # Let Home Manager install and manage itself.
   home-manager.enable = true;
 
+  broot = {
+    enable = true;
+    enableZshIntegration = true;
+  };
   direnv = import ./direnv { inherit pkgs; };
   git = import ./git { inherit pkgs; };
   tmux = import ./tmux { inherit pkgs; };
