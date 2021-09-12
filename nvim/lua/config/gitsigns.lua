8525c4a1db000a32aac27d59cec4d04e64c5e2e5
bangn <thebangnguyen@gmail.com>
2021-09-12 17:03:34 +1000

    vim: Update gitsigns options

--- nvim/lua/config/gitsigns.lua
+++ nvim/lua/config/gitsigns.lua
@@ -30,8 +30,10 @@ require("gitsigns").setup({
 	watch_index = {
 		interval = 1000,
 	},
+	diff_opts = {
+		internal = true,
+	},
 	sign_priority = 6,
 	update_debounce = 100,
 	status_formatter = nil, -- Use default
-	use_internal_diff = true, -- If luajit is present
 })
