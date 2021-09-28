e145f943c08bf52a1931310d6a692c4ca7b1dc14
bangn-ap <bang.nguyen@afterpay.com>
2021-09-29 09:44:55 +1000

    nix-darwin: Add afterpay script

--- nixpkgs/programs/zsh/initExtra/default.nix
+++ nixpkgs/programs/zsh/initExtra/default.nix
@@ -1,10 +1,12 @@
 let
+  afterpay = builtins.readFile ./afterpay.sh;
   functions = builtins.readFile ./functions.sh;
   git = builtins.readFile ./git.sh;
   keybindings = builtins.readFile ./keybindings.sh;
   zshrc = builtins.readFile ./zshrc.sh;
   prompt = builtins.readFile ./prompt.sh;
 in
+afterpay +
 functions +
 git +
 keybindings +
