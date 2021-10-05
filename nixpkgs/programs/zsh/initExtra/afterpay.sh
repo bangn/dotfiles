effe54358e123861e98010501737b7dcda209863
bangn-ap <bang.nguyen@afterpay.com>
2021-10-05 23:04:13 +1100

    zsh[afterpay]: Add zsh function to spin up/stop db containers

--- nixpkgs/programs/zsh/initExtra/afterpay.sh
+++ nixpkgs/programs/zsh/initExtra/afterpay.sh
@@ -6,36 +6,53 @@
 
 # Theses functions is tightly couple with tmux
 # I dont care much about the coupling here as tmux is used daily in my workflow
-nissup() {
-  up_with="$1"
-  session=niss
-  ignore
+# nissup() {
+#   up_with="$1"
+#   session=niss
+#   ignore
 
-  if [ -z "$up_with" ] || [ "$up_with" = "ui" ]; then
-    ignore="niss-admin"
-  else
-    ignore="niss-ui"
-  fi
+#   if [ -z "$up_with" ] || [ "$up_with" = "ui" ]; then
+#     ignore="niss-admin"
+#   else
+#     ignore="niss-ui"
+#   fi
 
-  for pane in $(tmux list-panes -t "$session" -F "#{pane_current_path}:#W.#P" | grep -v "$ignore"); do
-    paneId=$(echo "$pane" | awk -F ":" '{print $2}')
-    # shellcheck disable=SC3010
-    if [[ "$pane" =~ "nailgun" ]]; then
-      tmux send-keys -t "$paneId" "be rackup -p 8080" Enter
-    else
-      tmux send-keys -t "$paneId" "npm run start.dev" Enter
-    fi
-  done
+#   for pane in $(tmux list-panes -t "$session" -F "#{pane_current_path}:#W.#P" | grep -v "$ignore"); do
+#     paneId=$(echo "$pane" | awk -F ":" '{print $2}')
+#     # shellcheck disable=SC3010
+#     if [[ "$pane" =~ "nailgun" ]]; then
+#       tmux send-keys -t "$paneId" "be rackup -p 8080" Enter
+#     else
+#       tmux send-keys -t "$paneId" "npm run start.dev" Enter
+#     fi
+#   done
 
-  echo "niss servers are starting up"
+#   echo "niss servers are starting up"
+# }
+
+# nissdown() {
+#   session=niss
+#   for pane in $(tmux list-panes -t "$session" -F "#{pane_current_path}:#W.#P"); do
+#     paneId=$(echo "$pane" | awk -F ":" '{print $2}')
+#     tmux send-keys -t "$paneId" C-C
+#   done
+
+#   echo "niss servers are down"
+# }
+
+MONEY_DIR="$HOME/dev/money"
+updb() {
+  # shellcheck disable=SC3044
+  pushd "$MONEY_DIR"
+  docker-compose up -d
+  # shellcheck disable=SC3044
+  popd
 }
 
-nissdown() {
-  session=niss
-  for pane in $(tmux list-panes -t "$session" -F "#{pane_current_path}:#W.#P"); do
-    paneId=$(echo "$pane" | awk -F ":" '{print $2}')
-    tmux send-keys -t "$paneId" C-C
-  done
-
-  echo "niss servers are down"
+stopdb() {
+  # shellcheck disable=SC3044
+  pushd "$MONEY_DIR"
+  docker-compose stop
+  # shellcheck disable=SC3044
+  popd
 }
