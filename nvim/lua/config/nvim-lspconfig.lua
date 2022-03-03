f7ecee1547731c85e73d21a70800f9a5e7815979
bangn-ap <bang.nguyen@afterpay.com>
2022-03-03 14:08:38 +1100

    vim: Update lspconfig

--- nvim/lua/config/nvim-lspconfig.lua
+++ nvim/lua/config/nvim-lspconfig.lua
@@ -3,12 +3,25 @@ local lspconfig = require("lspconfig")
 local vars = require("utils.vars")
 local vim_api = vim.api
 
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
 	vim_api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 	-- Mappings.
 	-- vars.remap.fn('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', vars.remap.opts)
 	-- vars.remap.fn('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', vars.remap.opts)
 	vars.remap.fn("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", vars.remap.opts)
+
+	if client.resolved_capabilities.document_highlight then
+		vim_api.nvim_exec(
+			[[
+                augroup lsp_document_highlight
+                    autocmd! * <buffer>
+                    autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                    autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+                augroup END
+            ]],
+			false
+		)
+	end
 end
 
 local servers = {
