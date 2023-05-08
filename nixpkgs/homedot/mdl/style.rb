bb78eaefa2df5a1827db8c92fbd065d5c5fd43ad
bnguyen-block <bnguyen@squareup.com>
2023-05-08 22:03:22 +1000

    mdl: Use 2 spaces to indent unorder list

--- nixpkgs/homedot/mdl/style.rb
+++ nixpkgs/homedot/mdl/style.rb
@@ -9,3 +9,4 @@ exclude_rule 'MD033' # No inline html
 exclude_rule 'MD041' # First line should be header - Ignore it for neuron
 
 rule 'MD024', allow_different_nesting: true # Multiple headers with the same content
+rule 'MD007', indent: 2
