63d221a25b960ab4424a5688e610469e8eca5698
bangn <thebangnguyen@gmail.com>
2023-11-05 19:29:51 +1100

    nix: Add ruby-version

--- nixpkgs/homedot/default.nix
+++ nixpkgs/homedot/default.nix
@@ -16,6 +16,8 @@
   ".rspec".source = ./ruby/.rspec;
   ".rubocop.yml".source = ./rubocop/.rubocop.yml;
   ".rufo".source = ./rufo/.rufo;
+  ".ruby-version".source = ./ruby/.ruby-version;
+
 
   # asdf dotfiles
   ".asdfrc".source = ./asdf/.asdfrc;
@@ -38,12 +40,13 @@
   ".mdlrc".source = ./mdl/.mdlrc;
   "mdl/style.rb".source = ./mdl/style.rb;
 
-  "dircolors.nord".source = pkgs.fetchFromGitHub {
-    owner = "arcticicestudio";
-    repo = "nord-dircolors";
-    rev = "master";
-    sha256 = "1c9fa6dip266z6hfqd5nan5v6qjp6dg074lvk4rxisirm26djlzz";
-  } + "/src/dir_colors";
+  "dircolors.nord".source = pkgs.fetchFromGitHub
+    {
+      owner = "arcticicestudio";
+      repo = "nord-dircolors";
+      rev = "master";
+      sha256 = "1c9fa6dip266z6hfqd5nan5v6qjp6dg074lvk4rxisirm26djlzz";
+    } + "/src/dir_colors";
 
   # Hammerspoon
   ".hammerspoon/init.lua".source = ./hammerspoon/init.lua;
@@ -59,9 +62,6 @@
 
   # ideavimrc
   ".ideavimrc".source = ./idea/ideavimrc;
-
-  # ruby-version
-  ".ruby-version".source = ./ruby-version/.ruby-version;
 
   # detekt
   "detekt.yaml".source = ./detekt/detekt.yaml;
