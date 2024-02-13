9223396e728ebc1e11aaf3b85048e50a0aa11d41
bangn <thebangnguyen@gmail.com>
2024-02-14 09:43:59 +1100

    homedot: Move .* files to * files to make them searchable

--- home-manager/homedot/default.nix
+++ home-manager/homedot/default.nix
@@ -1,52 +1,50 @@
 { pkgs, ... }: {
-  ".editrc".source = ./.editrc;
+  ".editrc".source = ./editrc;
   ".gitmessage.sample".source = ./git/gitmessage.sample;
-  ".inputrc".source = ./.inputrc;
-  ".psqlrc".source = ./psql/.psqlrc;
-  ".pylintrc".source = ./python/.pylintrc;
-  ".xsessionrc".source = ./xsession/.xsessionrc;
+  ".inputrc".source = ./inputrc;
+  ".psqlrc".source = ./psql/psqlrc;
+  ".pylintrc".source = ./python/pylintrc;
+  ".xsessionrc".source = ./xsession/xsessionrc;
 
   # xinput
-  ".xinitrc".source = ./xinput/.xinitrc;
-  ".xinputrc".source = ./xinput/.xinputrc;
+  ".xinitrc".source = ./xinput/xinitrc;
+  ".xinputrc".source = ./xinput/xinputrc;
 
   # Ruby dotfiles
-  ".irbrc".source = ./ruby/.irbrc;
-  ".pryrc".source = ./ruby/.pryrc;
-  ".rspec".source = ./ruby/.rspec;
-  ".rubocop.yml".source = ./rubocop/.rubocop.yml;
-  ".rufo".source = ./rufo/.rufo;
-  ".ruby-version".source = ./ruby/.ruby-version;
-
+  ".irbrc".source = ./ruby/irbrc;
+  ".pryrc".source = ./ruby/pryrc;
+  ".rspec".source = ./ruby/rspec;
+  ".rubocop.yml".source = ./rubocop/rubocop.yml;
+  ".rufo".source = ./rufo/rufo;
+  ".ruby-version".source = ./ruby/ruby-version;
 
   # asdf dotfiles
-  ".asdfrc".source = ./asdf/.asdfrc;
-  ".default-gems".source = ./asdf/.default-gems;
-  ".default-npm-packages".source = ./asdf/.default-npm-packages;
+  ".asdfrc".source = ./asdf/asdfrc;
+  ".default-gems".source = ./asdf/default-gems;
+  ".default-npm-packages".source = ./asdf/default-npm-packages;
   ".tool-versions".source = ./asdf/tool-versions;
 
   # haskell dotfiles
-  ".ghci".source = ./haskell/.ghci;
-  ".haskeline".source = ./haskell/.haskeline;
-  ".hindent.yaml".source = ./haskell/.hindent.yaml;
-  ".stylish-haskell.yaml".source = ./haskell/.stylish-haskell.yaml;
-  ".summoner.toml".source = ./haskell/.summoner.toml;
+  ".ghci".source = ./haskell/ghci;
+  ".haskeline".source = ./haskell/haskeline;
+  ".hindent.yaml".source = ./haskell/hindent.yaml;
+  ".stylish-haskell.yaml".source = ./haskell/stylish-haskell.yaml;
+  ".summoner.toml".source = ./haskell/summoner.toml;
   "floskell.json".source = ./haskell/floskell.json;
 
   # tmuxinantor
   ".tmuxinator/personal.yml".source = ./tmuxinator/personal.yml;
 
   # mdl
-  ".mdlrc".source = ./mdl/.mdlrc;
+  ".mdlrc".source = ./mdl/mdlrc;
   "mdl/style.rb".source = ./mdl/style.rb;
 
-  "dircolors.nord".source = pkgs.fetchFromGitHub
-    {
-      owner = "arcticicestudio";
-      repo = "nord-dircolors";
-      rev = "master";
-      sha256 = "1c9fa6dip266z6hfqd5nan5v6qjp6dg074lvk4rxisirm26djlzz";
-    } + "/src/dir_colors";
+  "dircolors.nord".source = pkgs.fetchFromGitHub {
+    owner = "arcticicestudio";
+    repo = "nord-dircolors";
+    rev = "master";
+    sha256 = "1c9fa6dip266z6hfqd5nan5v6qjp6dg074lvk4rxisirm26djlzz";
+  } + "/src/dir_colors";
 
   # Hammerspoon
   ".hammerspoon/init.lua".source = ./hammerspoon/init.lua;
