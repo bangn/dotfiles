4f86c94f269855d8b0aa24623dd9d268eb62017d
Bang Nguyen <bnguyen@squareup.com>
2024-03-26 13:57:24 +1100

    nix: Add pip config

--- home-manager/config/default.nix
+++ home-manager/config/default.nix
@@ -4,13 +4,14 @@
   else
     ./alacritty/alacritty-macos.toml;
   ".config/brittany/brittany.yml".source = ./brittany/config.yaml;
+  ".config/dunst/dunstrc".source = ./dunst/dunstrc;
   ".config/i3/config".source = ./i3/config;
   ".config/i3status/config".source = ./i3status/config;
+  ".config/nix/nix.conf".source = ./nix/nix.conf;
+  ".config/pip/pip.conf".source = ./pip/pip.conf;
   ".config/rofi/config.rasi".source = ./rofi/config.rasi;
   ".config/rofi/custom-nord.rasi".source = ./rofi/custom-nord.rasi;
+  ".config/starship.toml".source = ./starship/starship.toml;
   ".config/yamllint/config".source = ./yamllint/config;
   ".config/zathura/zathurarc".source = ./zathura/zathurarc;
-  ".config/starship.toml".source = ./starship/starship.toml;
-  ".config/dunst/dunstrc".source = ./dunst/dunstrc;
-  ".config/nix/nix.conf".source = ./nix/nix.conf;
 }
