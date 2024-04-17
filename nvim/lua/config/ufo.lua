d53d91d57c195c019c6cc913df6b494382801607
bangn <thebangnguyen@gmail.com>
2024-04-17 14:39:45 +1000

    vim: Update ufo config to use treesitter

--- nvim/lua/config/ufo.lua
+++ nvim/lua/config/ufo.lua
@@ -4,20 +4,12 @@ vim_wo.foldcolumn = "1"
 vim_wo.foldlevel = 15
 vim_wo.foldenable = true
 
-local ftMap = {
-	vim = "indent",
-	git = "",
-}
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
-local bakup = jit
-jit = nil
 require("ufo").setup({
-	provider_selector = function(bufnr, filetype)
-		-- return a string type use internal providers
-		-- return a string in a table like a string type
-		-- return empty string '' will disable any providers
-		-- return `nil` will use default value {'lsp', 'indent'}
-		return ftMap[filetype]
+	provider_selector = function(bufnr, filetype, buftype)
+		return { "treesitter", "indent" }
 	end,
 })
-jit = bakup
