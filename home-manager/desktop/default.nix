ea14dcb9f18606e4adc9fa30da8e41604b220937
bangn <thebangnguyen@gmail.com>
2024-10-03 02:38:17 +1000

    nix: Add CursorAI desktop file

--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -1,8 +1,4 @@
 { pkgs, ... }: {
   ".local/share/applications/Alacritty.desktop".source = ./Alacritty.desktop;
-  ".local/share/applications/firefox.desktop".source = ./firefox.desktop;
-  ".local/share/applications/sxiv.desktop".source = ./sxiv.desktop;
-  ".local/share/applications/thunderbird.desktop".source =
-    ./thunderbird.desktop;
-  ".local/share/applications/zathura.desktop".source = ./zathura.desktop;
+  ".local/share/applications/CursorAI.desktop".source = ./CursorAI.desktop;
 }
