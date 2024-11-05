b3d655b497bfe9daf37c4a347b73e119fafec94f
bangn <thebangnguyen@gmail.com>
2024-11-05 15:24:42 +1100

    mdl: Ignore MD029 rule
    Ordered list item prefix.

--- home-manager/homedot/mdl/style.rb
+++ home-manager/homedot/mdl/style.rb
@@ -4,7 +4,9 @@ all
 
 exclude_rule 'MD004' # Unordered list style
 exclude_rule 'MD013' # Line length
+exclude_rule 'MD029' # Ordered list item prefix
 exclude_rule 'MD032' # Lists should be surrounded by blank lines
+exclude_rule 'MD033' # No inline html
 exclude_rule 'MD033' # No inline html
 
 rule 'MD024', allow_different_nesting: true # Multiple headers with the same content
