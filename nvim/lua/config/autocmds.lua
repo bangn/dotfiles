79a238376ca0725e4fc36eb7e284f73e807c9117
bangn <thebangnguyen@gmail.com>
2024-11-17 00:50:32 +1100

    nvim: Use lazyvim config

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+<<<<<<< HEAD
 
 local autocmd = vim.api.nvim_create_autocmd
 
@@ -12,3 +13,6 @@ autocmd({ "BufLeave", "FocusLost" }, {
   command = "silent! wall",
   nested = true,
 })
+||||||| parent of ebf9337 (nvim: Use lazyvim config)
+=======
+>>>>>>> ebf9337 (nvim: Use lazyvim config)
