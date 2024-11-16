9eb78885ac037af0c2d76e5f172728231029be23
bangn <thebangnguyen@gmail.com>
2024-11-17 01:22:59 +1100

    nvim: Update lazyvim config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-<<<<<<< HEAD
 
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
@@ -52,33 +51,6 @@ require("lazy").setup({
         "matchit",
         "matchparen",
         "netrwPlugin",
-||||||| parent of ebf9337 (nvim: Use lazyvim config)
-=======
-    { import = "plugins" },
-  },
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      -- disable some rtp plugins
-      disabled_plugins = {
-        "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
->>>>>>> ebf9337 (nvim: Use lazyvim config)
         "tarPlugin",
         "tohtml",
         "tutor",
--- nvim/lua/plugins/tpope.lua
+++ nvim/lua/plugins/tpope.lua
@@ -2,13 +2,7 @@ return {
   { "tpope/vim-abolish", lazy = false },
   { "tpope/vim-endwise", lazy = false },
   { "tpope/vim-eunuch", lazy = false },
-<<<<<<< HEAD
   { "tpope/vim-repeat", lazy = false },
   { "tpope/vim-surround", lazy = false },
-||||||| parent of ebf9337 (nvim: Use lazyvim config)
-=======
-  { "tpope/vim-fugitive", lazy = false },
-  { "tpope/vim-repeat", lazy = false },
->>>>>>> ebf9337 (nvim: Use lazyvim config)
   { "tpope/vim-unimpaired", lazy = false },
 }
