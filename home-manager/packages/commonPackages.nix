04e0ba84e72403be8788623ad84b5fedb22dc194
Bang Nguyen <bnguyen@squareup.com>
2024-11-18 15:16:46 +1100

    nvim: Add markdown cli

--- home-manager/packages/commonPackages.nix
+++ home-manager/packages/commonPackages.nix
@@ -35,11 +35,17 @@ with pkgsUnstable; [
   luajitPackages.luacheck
   luarocks
 <<<<<<< HEAD
+<<<<<<< HEAD
   markdownlint-cli
   markdownlint-cli2
 ||||||| parent of 47a5d2e (nvim: Update lazyvim config)
 =======
 >>>>>>> 47a5d2e (nvim: Update lazyvim config)
+||||||| parent of 0e88b7c (nvim: Add markdown cli)
+=======
+  markdownlint-cli
+  markdownlint-cli2
+>>>>>>> 0e88b7c (nvim: Add markdown cli)
   mdl
   minikube
   mise
