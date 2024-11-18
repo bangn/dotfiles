400f2bc57df79bbefdc17f5b239052bd9e160dca
Bang Nguyen <bnguyen@squareup.com>
2024-11-18 16:51:42 +1100

    nvim: Auto start coq correctly

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-<<<<<<< HEAD
 
 -- COQ settings
 vim.g.coq_settings = {
@@ -11,6 +10,3 @@ vim.g.coq_settings = {
     jump_to_mark = "<C-f>",
   },
 }
-||||||| parent of ebf9337 (nvim: Use lazyvim config)
-=======
->>>>>>> ebf9337 (nvim: Use lazyvim config)
