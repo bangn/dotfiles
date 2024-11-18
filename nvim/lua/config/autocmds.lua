2b67333bae290d13d46aa0e812061e193edb9e06
Bang Nguyen <bnguyen@squareup.com>
2024-11-18 16:07:42 +1100

    nvim: Add autosave autocmds

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+----------------------------------------
+--- Auto save
+----------------------------------------
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  pattern = { "*" },
+  command = "silent! wall",
+  nested = true,
+})
