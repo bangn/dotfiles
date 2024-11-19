68eab8f2e5a9cfc392b4dc640223149c1b5909ca
Bang Nguyen <bnguyen@squareup.com>
2024-11-19 13:11:14 +1100

    nvim: Set conceallevel correctly

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,7 +2,6 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 ----------------------------------------
@@ -12,16 +11,4 @@ autocmd({ "InsertLeave" }, {
   pattern = { "*" },
   command = "silent! wall",
   nested = true,
-})
-
--- Prefer creating groups and assigning autocmds to groups, because it makes it
--- easier to clear them
-augroup("conceal", { clear = true })
-
-autocmd({ "FileType" }, {
-  pattern = { "markdown", "json" },
-  callback = function()
-    vim.wo.conceallevel = 0
-  end,
-  group = "conceal",
 })
