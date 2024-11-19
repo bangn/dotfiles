977423ec17c8ac980216dca7053f18afad8eba53
Bang Nguyen <bnguyen@squareup.com>
2024-11-19 13:31:48 +1100

    nvim: Move disabled plugins to disabled.lua; update which-key config

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,17 +2,16 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts_extend = { "spec" },
-  opts = {
-    spec = {
-      ["<leader>gb"] = { name = "+git blame" },
-      ["<leader>md"] = { name = "+markdown" },
-      ["<leader>a"] = { name = "Apply marcro" },
-    },
-  },
-  keys = {
-    { "<leader>qx",  ":x<CR>",                         desc = "Quit current window" },
-    { "<leader>rn",  ":set relativenumber!<CR>",       desc = "Set relativenumber" },
-    { "<leader>mdp", "<Cmd>MarkdownPreview<CR>",       desc = "Preview markdown" },
-    { "<leader>mdr", "<Cmd>RenderMarkdown toggle<CR>", desc = "Render markdown" },
-  }
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>rn",  ":set relativenumber!<CR>", desc = "Set relativenumber" },
+
+      { "<leader>md",  group = "markdown" },
+      { "<leader>mdp", "<Cmd>MarkdownPreview<CR>", desc = "Preview markdown" },
+
+      { "<leader>a",   group = "Apply macro" }
+    })
+    wk.setup(opts)
+  end,
 }
