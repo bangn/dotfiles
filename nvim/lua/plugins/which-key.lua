e9cf174546969d761b11f69767d66340d08deee6
Bang Nguyen <bnguyen@squareup.com>
2024-11-19 13:31:48 +1100

    nvim: Move disabled plugins to disabled.lua; update which-key config

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,7 +2,6 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts_extend = { "spec" },
-<<<<<<< HEAD
   config = function(_, opts)
     local wk = require("which-key")
     wk.add({
@@ -15,20 +14,4 @@ return {
     })
     wk.setup(opts)
   end,
-||||||| parent of 47a5d2e (nvim: Update lazyvim config)
-=======
-  opts = {
-    spec = {
-      ["<leader>gb"] = { name = "+git blame" },
-      ["<leader>md"] = { name = "+markdown" },
-      ["<leader>a"] = { name = "Apply marcro" },
-    },
-  },
-  keys = {
-    { "<leader>qx",  ":x<CR>",                         desc = "Quit current window" },
-    { "<leader>rn",  ":set relativenumber!<CR>",       desc = "Set relativenumber" },
-    { "<leader>mdp", "<Cmd>MarkdownPreview<CR>",       desc = "Preview markdown" },
-    { "<leader>mdr", "<Cmd>RenderMarkdown toggle<CR>", desc = "Render markdown" },
-  }
->>>>>>> 47a5d2e (nvim: Update lazyvim config)
 }
