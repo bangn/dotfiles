5078bad462bac34426cd92e17f16af5b279d9e9a
bangn <thebangnguyen@gmail.com>
2024-11-20 14:39:24 +1100

    nvim: Use vim-surround instead of mini.surround

--- nvim/lua/plugins/tpope.lua
+++ nvim/lua/plugins/tpope.lua
@@ -3,5 +3,6 @@ return {
   { "tpope/vim-endwise",    lazy = false },
   { "tpope/vim-eunuch",     lazy = false },
   { "tpope/vim-repeat",     lazy = false },
+  { "tpope/vim-surround",   lazy = false },
   { "tpope/vim-unimpaired", lazy = false },
 }
