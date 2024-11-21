f20c8357bbd213f310ba8c0bb73f613d7b4eac9a
bangn <thebangnguyen@gmail.com>
2024-11-21 13:50:46 +1100

    Revert "tmux: Use vim-tmux-navigator plugin"
    This reverts commit 5d8a6cc5fa1dcd1f269662b4e5d13dd7409b9095.

--- home-manager/programs/tmux/extraConfig.nix
+++ home-manager/programs/tmux/extraConfig.nix
@@ -3,7 +3,6 @@
   # Plugins
   ################################################################################
   set -g @plugin "arcticicestudio/nord-tmux"
-  set -g @plugin 'christoomey/vim-tmux-navigator'
 
   ################################################################################
   # Config
@@ -91,6 +90,29 @@
   # prefix + z: maximize current pane. Do it again to toggle back
   ####################
   bind = select-layout even-vertical
+
+  ####################
+  # Vim style pane selection
+  ####################
+  is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+      | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|\.?n?vim?x?(-wrapped)?)(diff)?$'"
+
+  is_fzf="ps -o state= -o comm= -t '#{pane_tty}' \
+    | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?fzf$'"
+
+  bind -n C-h run "($is_vim && tmux send-keys C-h) || \
+                   tmux select-pane -L"
+
+  bind -n C-j run "($is_vim && tmux send-keys C-j)  || \
+                   ($is_fzf && tmux send-keys C-j) || \
+                   tmux select-pane -D"
+
+  bind -n C-k run "($is_vim && tmux send-keys C-k) || \
+                   ($is_fzf && tmux send-keys C-k)  || \
+                   tmux select-pane -U"
+
+  bind -n C-l run "($is_vim && tmux send-keys C-l) || \
+                   tmux select-pane -R"
 
   bind C-\\ if-shell "$is_vim" "send-keys C-\\" "select-pane -l"
   bind C-l send-keys 'C-l' # Alternate mapping to clear-screen
