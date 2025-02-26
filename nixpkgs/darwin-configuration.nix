92fe7b541080684874f0a7c821d5873d5f537186
Bang Nguyen <bnguyen@squareup.com>
2025-02-27 10:34:32 +1100

    nix-darwin: Update deprecated setting

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -28,7 +28,7 @@ in {
   };
 
   # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 
   imports = [
     (import ./darwin/preferences.nix {inherit config pkgs homeDirectory;})
