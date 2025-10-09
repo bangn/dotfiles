b88b15214638a1aa39aee6bc3dfaf6962d01c1ab
bangn <thebangnguyen@gmail.com>
2025-10-09 15:23:37 +1100

    jj: Update jj config

--- home-manager/programs/jujutsu/default.nix
+++ home-manager/programs/jujutsu/default.nix
@@ -15,10 +15,7 @@
       color = "auto"; # "always", "never", or "auto"
       default-command = "log"; # Default command if none is specified
       pager = "delta";
-      diff = {
-        tool = "delta"; # Specify diff tool ("git", "delta", etc.)
-        format = "git";
-      };
+      diff-formatter = "delta"; # Specify diff tool ("git", "delta", etc.)
     };
   };
 }
