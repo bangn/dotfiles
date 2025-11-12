3aa66d84eb6b3210a2443b400276bbe68114ee88
Bang Nguyen <bnguyen@squareup.com>
2025-11-12 13:22:28 +1100

    git: Add worktree alias

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -21,7 +21,9 @@ in {
       root = "!pwd";
       squash-all = "!f(){ git reset $(git commit-tree HEAD^{tree} -m \${1:-Initial commit});};f";
       tree = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+      wt = "worktree";
     };
+  };
 
     advice = {skippedCherryPicks = false;};
     branch = {
