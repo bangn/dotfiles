735e1fe75506591c4f5f67c381cb4f62641ccf5c
Bang Nguyen <bnguyen@squareup.com>
2025-11-13 14:07:24 +1100

    vim: Try render-markdown

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -2,5 +2,5 @@ return {
   { "blink.cmp", enabled = false },
   { "coq_nvim", enabled = true },
   { "nvim-cmp", enabled = false },
-  { "render-markdown.nvim", enabled = false },
+  { "render-markdown.nvim", enabled = true },
 }
